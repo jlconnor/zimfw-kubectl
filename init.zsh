ffa5fbb18f51549f40d12a0c67d6c88ac6f48752
Jason L Connor <jason.connor@gmail.com>
2025-04-28 15:04:39 -0600

    Kubectl support

--- init.zsh
+++ init.zsh
@@ -1,4 +1,11 @@
-# This is example code.
-# This will be executed when this module is initialized during Zsh startup.
+(( ${+commands[kubectl]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
 
-print "executed code in ${0}"
+  local command=${commands[kubectl]:-"$(${commands[asdf]} which kubectl 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  local compfile=$1/functions/_kubectl
+  if [[ ! -e $compfile || $compfile -ot $command ]]; then
+    $command completion zsh >| $compfile
+    print -u2 -PR "* Detected a new version 'kubectl'. Regenerated completions."
+  fi
+} ${0:h}
